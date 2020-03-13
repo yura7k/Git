ce00c5d860753ba13eaed1cf5921f394cb8fd805
yura7k <yura7kjob@gmail.com>
2020-03-13 13:54:58 +0200

    Add Task 4

---
+++ Python_4_tasks/4.CharPrevNext.py
@@ -0,0 +1,24 @@
+""" Program output previos and next elements of input symbol
+
+    Input:
+        symbol - any symbol
+
+    Print:
+        Previos symbol
+        Curent symbol
+        Next symbol
+        Or Error if enter many symbol
+
+    """
+while True:
+    try:
+        symbol = input("Input any symbol: ")
+        symbol = ord(symbol)
+
+        print("Previos symbol is ", chr(symbol-1))
+        print("Curent symbol is ", chr(symbol))
+        print("Next symbol is ", chr(symbol+1))
+
+        break
+    except Exception:
+        print("You print so many symbols! Try again")
