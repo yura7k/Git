3b9b25f6b34bcd318b1d228b1e459bfad99d0aa5
yura7k <yura7kjob@gmail.com>
2020-03-17 16:44:59 +0200

    Add Level 1 Lesson 7

---
+++ Python_7_tasks/Task_1.py
@@ -0,0 +1,55 @@
+""" Функція парсить JSON файл та повертає свисок, кожен елемент якого є словником (dict)
+
+    Input:
+        file - шлях до файлу
+
+    Output:
+        listCar (list) - де елементи списку словник (dict) одного елементу JSON
+    """
+def parceJson(file = "D:/Python/Git/python_course/Python_7_tasks/Car_Model_List.json"):
+    #  завантажуємо текст з підготовленого файлу
+    try:
+        fileExemple = open(file)
+        strExemple = fileExemple.read()
+    except EOFError:
+        print("Open open file")
+    finally:
+        fileExemple.close()
+    strExemple = strExemple.strip('[]')
+    listTmp = strExemple.splitlines()
+    listCar = []
+    tempDict = {}
+    for item in listTmp:
+        item = item.strip()
+        if item == '{' or item == '':
+            continue
+        elif item != '},':
+            item = item.partition(':')
+            keyDict = item[0].strip(' ",')
+            itemDict = item[2].strip(' ",')
+            tempDict[keyDict] = itemDict
+        else:
+            listCar.append(tempDict)
+            tempDict = {}
+    return listCar
+
+#  друкуємо заголовок таблички
+lineTop = ("+" + "".center(10, '-') + "+" + "-".center(20, "-") + "+" + 
+        "-".center(35, "-") + "+" + "-".center(40, "-") +"+")
+print(lineTop)
+print("|" + "Year".center(10) + "|" + "Make".center(20) + "|" + 
+        "Model".center(35) + "|" + "Category".center(40) +"|")
+print(lineTop)
+
+#  Парсим наш json
+tmrJson = parceJson()
+listTMP = []
+
+for item in tmrJson:
+    if item.get('Year') == '2019': # вивести список авто 2020 року
+        print(lineTop)
+        break
+    lineCar = ("|" + item.get('Year').center(10) + "|" + item.get('Make').center(20) + "|" + 
+        item.get('Model').center(35) + "|" + item.get('Category').center(40) +"|")
+    print(lineCar)
+    #  listTMP.append(lineCar)
