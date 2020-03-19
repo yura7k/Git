a3a7591c000df315b9c8090d5ace40285eee6a3d
yura7k <yura7kjob@gmail.com>
2020-03-19 21:15:54 +0200

    Add Task 3, 4 Level 2

--- Python_7_tasks/ParserJSON.py
+++ Python_7_tasks/ParserJSON.py
@@ -24,6 +24,9 @@ def openFile(file= fileName):
     finally:
         fileData.close()
 
+#  parce JSON file to Python list
+parceJSON = openFile()
+
 """ Function prints the data as a table
     Input:
         listData (list) - list of data
@@ -39,8 +42,8 @@ def printTable(dataToPrint):
         listItem = dataToPrint[0]
         if isinstance(listItem, dict):
             for item in listItem.keys():
-                lineTop = lineTop + ("".center(25, '-') + "+")
-                lineTitle = lineTitle + item.center(25) + "|"
+                lineTop = lineTop + ("".center(30, '-') + "+")
+                lineTitle = lineTitle + item.center(30) + "|"
             print(lineTop)
             print(lineTitle)
             print(lineTop)
@@ -48,7 +51,7 @@ def printTable(dataToPrint):
                 for item in data:
                     if type(data[item]) == int:
                         data[item] = str(data[item])
-                    lineData = lineData + data[item].center(25) + "|"
+                    lineData = lineData + data[item].center(30) + "|"
                 print(lineData)
                 lineData = "|"
         else:
@@ -64,7 +67,6 @@ def printTable(dataToPrint):
 """ Level 2 - Task1 - Return the list of car brands
 
     Input:
-        file (str) - file name
         sort (str) - sort list by:
             none - no sorting
             ask - sort up
@@ -73,59 +75,142 @@ def printTable(dataToPrint):
     Output:
         listBrand (list) - list All brands
     """
-def listBrands(sort = "none"):
-    
-    listJSON = openFile()
+def listBrands(data= parceJSON, sort = "none"):
+    listJSON = data
     listBrand = []
 
-    for item in listJSON:
-        if item['Make'] in listBrand:
-            continue
-        else:
-            listBrand.append(item['Make'])
+    if len(listJSON) < 1:
+        listBrand.append("ERROR: No input data!")
+        return listBrand
+    else:
+        for item in listJSON:
+            if item['Make'] in listBrand:
+                continue
+            else:
+                listBrand.append(item['Make'])
 
-    if sort == 'ask':
-        listBrand.sort()
-    elif sort == 'desk':
-        listBrand.sort(reverse=True)
+        if sort == 'ask':
+            listBrand.sort()
+        elif sort == 'desk':
+            listBrand.sort(reverse=True)
 
-    return listBrand
+        return listBrand
 
-""" Level 2 - Task2 - Return the list of models by input brands
+""" Level 2 - Task 2 - Return the list of models by input brands
 
     Input:
-        file (str) - file name
         brand (str) - name brand
 
     Output:
         listOut (list) - list All models by input brands
     """
-def listModelByBrand(brand = "Tesla"):
-    listJSON = openFile()
+def listModelByBrand(data= parceJSON, brand = "Tesla"):
+    listJSON = data
     listOut = []
-    
-    for item in listJSON:
-        dictModel = {}
-        if item['Make'].lower() == brand.lower():
-            dictModel['Make'] = item['Make']
-            dictModel['Model'] = item['Model']
-            dictModel['Year'] = str(item['Year'])
-            listOut.append(dictModel)
-    if len(listOut) < 1:
-        listOut.append("ERROR: wrong brand - " + brand)
+
+    if len(listJSON) < 1:
+        listOut.append("ERROR: No input data!")
+        return listOut
+    else:    
+        for item in listJSON:
+            dictModel = {}
+            if brand.lower() in item['Make'].lower():
+                dictModel['Make'] = item['Make']
+                dictModel['Model'] = item['Model']
+                dictModel['Year'] = str(item['Year'])
+                listOut.append(dictModel)
+        if len(listOut) < 1:
+            listOut.append("ERROR: wrong brand - " + brand)
+
+        return listOut
+
+""" Level 2 - Task 3 - Return the list of models by input name
+
+    Input:
+        model (str) - seach model
+
+    Output:
+        listOut (list) - list All models by seach model
+    """
+def listModelByModel(data= parceJSON, model= "Model X"):
+    listJSON = data
+    listOut = []
+
+    if len(listJSON) < 1:
+        listOut.append("ERROR: No input data!")
+        return listOut
+    else:    
+        for item in listJSON:
+            dictModel = {}
+            if model.lower() in item['Model'].lower():
+                dictModel['Make'] = item['Make']
+                dictModel['Model'] = item['Model']
+                dictModel['Year'] = str(item['Year'])
+                listOut.append(dictModel)
+        if len(listOut) < 1:
+            listOut.append("ERROR: wrong model - " + model)
 
     return listOut
 
-# Level 2 - Task1
-dataBrands = listBrands(sort='ask')
-printTable(dataToPrint= dataBrands)
+""" Level 2 - Task 4 - Return the list of models by input year
 
-# Level 2 - Task2
+    Input:
+        Year (str) - seach Year
+
+    Output:
+        listOut (list) - list All models by seach year
+    """
+def listModelByYear(data= parceJSON, year= "2019"):
+    listJSON = data
+    listOut = []
+    listYear = []
+
+    if ',' in year:
+        listYear = year.split(',')
+    elif '-' in year:
+        year = year.split('-')
+        for i in range(int(year[0]), int(year[1]) + 1):
+            #  через те що на 187 стрічці, я конвертую в str
+            #  довелось конвертувати і тут щоб не було конфлікту
+            listYear.append(str(i))
+    else:
+        listYear.append(year)
+
+    if len(listJSON) < 1:
+        listOut.append("ERROR: No input data!")
+        return listOut
+    else:    
+        for itemYear in listYear:
+            for item in listJSON:
+                dictModel = {}
+                if itemYear.lower() in item['Year'].lower():
+                    dictModel['Make'] = item['Make']
+                    dictModel['Model'] = item['Model']
+                    dictModel['Year'] = str(item['Year'])
+                    listOut.append(dictModel)
+        if len(listOut) < 1:
+            listOut.append("ERROR: wrong model - " + year)
+
+    return listOut
+
+# Level 2 - Task 1
+#dataBrands = listBrands(sort='ask')
+#printTable(dataToPrint= dataBrands)
+
+# Level 2 - Task 2
 brand = input("Input name of brand auto: ")
 dataModels = listModelByBrand(brand= brand)
 printTable(dataToPrint= dataModels)
 
-# Test Data
-# listBrands(sort = 'desk')
-# data = [{"Make": "Audi", "Model": "Q3", "Category": "SUV"},{"Make": "Chevrolet", "Model": "Malibu", "Category": "Sedan"}]
+# Level 2 - Task 3 
+model = input("Input model auto: ")
+dataModels = listModelByModel(data= dataModels, model= model)
+printTable(dataToPrint= dataModels)
 
+# Level 2 - Task 4
+# Працює все послідовно, тобто відбирає  спочатку бренд
+# Потім модель
+# Потім по року
+inYear = input("Input Yaer of model auto (sep by ',' or '-'): ")
+dataModels = listModelByYear(data= dataModels, year= inYear)
+printTable(dataToPrint= dataModels)
--- Python_7_tasks/test.py
+++
@@ -1,3 +0,0 @@
-dirJson = "D:/Python/Git/python_course/Python_7_tasks/"
-file = "Car_Model_List.json"
-print(dirJson + file)
