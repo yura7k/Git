7af1e0b638205e1737d79c971093642df4aadb4b
yura7k <yura7kjob@gmail.com>
2020-03-24 10:21:59 +0200

    Add Task 2 Level 3 Lesson 7

--- Python_7_tasks/ParserJSON.py
+++ Python_7_tasks/ParserJSON.py
@@ -29,7 +29,7 @@ parceJSON = openFile()
 
 """ Function prints the data as a table
     Input:
-        listData (list) - list of data
+        dataToPrint (list) - list of data
     """
 def printTable(dataToPrint):
     if len(dataToPrint) < 1:
@@ -67,6 +67,7 @@ def printTable(dataToPrint):
 """ Level 2 - Task1 - Return the list of car brands
 
     Input:
+        data (list) - list of data
         sort (str) - sort list by:
             none - no sorting
             ask - sort up
@@ -99,6 +100,7 @@ def listBrands(data= parceJSON, sort = "none"):
 """ Level 2 - Task 2 - Return the list of models by input brands
 
     Input:
+        data (list) - list of data
         brand (str) - name brand
 
     Output:
@@ -124,9 +126,10 @@ def listModelByBrand(data= parceJSON, brand = "Tesla"):
 
         return listOut
 
-""" Level 2 - Task 3 - Return the list of models by input name
+""" Level 2 - Task 3 - Return the list of models by input model name
 
     Input:
+        data (list) - list of data
         model (str) - seach model
 
     Output:
@@ -153,8 +156,8 @@ def listModelByModel(data= parceJSON, model= "Model X"):
     return listOut
 
 """ Level 2 - Task 4 - Return the list of models by input year
-
     Input:
+        data (list) - list of data
         Year (str) - seach Year
 
     Output:
@@ -189,9 +192,41 @@ def listModelByYear(data= parceJSON, year= "2019"):
                     dictModel['Year'] = str(item['Year'])
                     listOut.append(dictModel)
         if len(listOut) < 1:
-            listOut.append("ERROR: wrong model - " + year)
+            listOut.append("ERROR: no model for - " + year)
 
     return listOut
+
+""" Level 3 - Task 2 - Print the list of models by page
+    Input:
+        data (list) - list of data
+        step (int) - count strings on print page
+        start (int) - first item for print
+    """
+def printByStep(data= parceJSON, step= 5, start = 0):
+    while True:
+        #  перевіряємо чи список більший за кількість елементів на сторінці
+        if len(data) > start + step:    
+            #  якщо так друкуємо першу сторінку і виводимо повідомлення
+            printTable(dataToPrint= data[start:start + step])
+            key = input("Press any <ENTER> to next or ANY SYMBOL to previos: ")
+            #  якщо немає символів друкуємо наступну сторінку
+            if key == '' and start >= 0:
+                firstEl = start + step
+                printByStep(data= data, step= step, start= firstEl)
+                break
+            #  інакше перевіряємо чи можливо надрукувати попередню сторінку
+            elif key != '' and start >= step:
+                firstEl = start - step
+                printByStep(data= data, step= step, start= firstEl)
+                break
+            #  якщо попередньої немає друкуємо помилку
+            else:
+                error = ["No previos page"]
+                printTable(dataToPrint= error)
+                break
+        else:
+            printTable(dataToPrint= data[start:])
+            break
 
 # Level 2 - Task 1
 #dataBrands = listBrands(sort='ask')
@@ -205,7 +240,10 @@ printTable(dataToPrint= dataModels)
 # Level 2 - Task 3 
 model = input("Input model auto: ")
 dataModels = listModelByModel(data= dataModels, model= model)
-printTable(dataToPrint= dataModels)
+# printTable(dataToPrint= dataModels)
+
+# Level 3 - Task 2 by step 
+printByStep(data= dataModels) 
 
 # Level 2 - Task 4
 # Працює все послідовно, тобто відбирає  спочатку бренд
