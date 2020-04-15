96b5bb1730f1ca49d91762816338cf2ec246e3b1
yura7k <yura7kjob@gmail.com>
2020-04-15 11:54:54 +0300

    Fix some bugs + add comments

--- Python_13_tasks/services/booking_service.py
+++ Python_13_tasks/services/booking_service.py
@@ -9,21 +9,33 @@ from helper.input_helper import (get_string, get_price, get_age, get_int, get_da
 from helper.output_helper import pretty_print
 
 class Booking_service():
+    """ View booking by Apartment name
+        
+        Input (str) -  name apartment
+        Output - print table Apt and table Bookings
+        """
     def booking_list(self):
         print(Fore.RED, " View Booking by Apartment ".center(30, "*"), Fore.RESET)
         columns_apt = ("Name", "price")
         columns_booking = ("guest_id", "chek_in_date", "chek_out_date", "booked_date")
 
+        #  Повертаємо апартаменти та друкуємо табличку
         Apartments = Apartment_service()
         apartments = Apartments.search_apartment()
 
         for apartment in apartments:
             guest_bookings = []
+
+            #  перевіряємо чи є бронювання
             if apartment.bookings:
                 for booking in apartment.bookings:
-                     guest_bookings.append(booking)
+                    #  Щоб не друкувати на кожне бронювання окрему табличку
+                    guest_bookings.append(booking)
+                print(Fore.YELLOW, "Booking(s) in " + apartment.name, Fore.RESET)
                 pretty_print(guest_bookings, columns_booking)
-
+            else:
+                print(Fore.YELLOW, "No booking(s) yet in " + apartment.name, Fore.RESET)
+             
 
     def booking_add(self):
         print(Fore.RED, " Add Booking ".center(30, "*"), Fore.RESET)
@@ -59,35 +71,40 @@ class Booking_service():
     
         print(Fore.RED, " Apartment Saved".center(30, "*"), Fore.RESET)
 
+
     def booking_seach_by_name(self):
+        """ View booking by Guest name
+        
+        Input (str) -  guest name
+        Output - print table Bookings
+        """
         print(Fore.RED, " Seach Booking by Name".center(30, "*"), Fore.RESET)
             
+        #  Повертаємо ID гостя
         guest = Guest_service()
         guestID = guest.search_guest()
-        
+        #  це друкую для виключно перевірки
         print(Fore.YELLOW, "You ID = ", guestID, Fore.RESET)
 
         columns_apt = ("Name", "price")
         columns_booking = ("guest_id", "chek_in_date", "chek_out_date", "booked_date")
-        guest_bookings = []
+        message = "You haven't bookings yet!"
 
+        #  Перевіряємо чи є бронювання
         for apartment in Apartment.objects():
-            # tmp = Apartment_service()
-            # tmp.search_apartment(name=apartment.name)
+            guest_bookings = []
+
+            #  Перевіряємо чи це бронювання нашого гостя
             for booking in apartment.bookings:
                 if booking.guest_id == guestID:
                     guest_bookings.append(booking)
 
-        if len(guest_bookings) > 0:
-            pretty_print(guest_bookings, columns_booking)
-        else:
-            print(Fore.YELLOW, "You hasn't bookings yet!", Fore.RESET)
-
-        #  Все працює, тобто виводить список бронювань гостя,
-        #  але, хочеться щоб виводило ще назву кімнати чи готелю, якщо є бронювання
-        #  пробував так - яле трохи не те
-        """ for apartment in Apartment.objects():
-            pretty_print([apartment], columns_apt)
-            for booking in apartment.bookings:
-                if booking.guest_id == guestID:
-                    pretty_print([booking], columns_booking)"""
+            #  якщо є бронювання друкуємо назву апартаментів та бронювання
+            if len(guest_bookings) > 0:
+                print(Fore.YELLOW, "You boking(s) in " + apartment.name, Fore.RESET)
+                pretty_print(guest_bookings, columns_booking)
+                message = ''
+        
+        #  якщо зовсім немає бронювань дкукуємо повідомлення
+        if message != '':
+            print(Fore.YELLOW, message, Fore.RESET)
